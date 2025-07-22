3fc3d1cefda2ae8d2b6de8b2d08d3a4b386b6297
Sgrpokhriyal2003 <sgrpokhriyal@gmail.com>
2025-07-22 16:58:38 +0530

    added mongodb connection

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-import express from 'express'
+import express, { urlencoded } from 'express'
+import session from 'express-session'
+import passport from 'passport'
 import dotenv from 'dotenv'
 dotenv.config()
+import morgan from 'morgan'
+import cors from 'cors'
+
+import connectDB from './src/config/db.js'
 
 const app = express()
 const PORT = process.env.PORT || 4000
+
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+    credentials: true
+}
+
+app.use(cors(corsOptions))
+app.use(express.json({limit: "100mb"}))
+app.use(urlencoded({limit: "100mb", extended: true}))
+app.use(session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 60000 * 60
+    }
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -11,6 +37,10 @@ app.get("/", (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is running on http://localhost:${PORT}`)
+    })
+}).catch((error) => {
+    console.log(error.message)
 })
